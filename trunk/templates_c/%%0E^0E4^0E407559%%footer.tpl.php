9ccfe1bc55010f3eaf1ee81c3f15ad74b4e7360e
dfd1985 <dfd1985@688d4dae-421b-0410-8eb5-f32591ffc86d>
2006-08-19 22:49:52 +0000

    git-svn-id: svn://svn.berlios.de/jclubcms@4 688d4dae-421b-0410-8eb5-f32591ffc86d

--- trunk/templates_c/%%0E^0E4^0E407559%%footer.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php /* Smarty version 2.6.13, created on 2006-05-06 11:31:17
-         compiled from footer.tpl */ ?>
-<div id="footerContainer">
-	<div id="footer">
-		&copy; by Schalk
-	</div>
-</div>
-</body>
-</html>
