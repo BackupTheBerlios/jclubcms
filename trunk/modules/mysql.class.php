b631930f5174c88bf4690070c04ce1dcf9c0ef5e
aristoteles <aristoteles@688d4dae-421b-0410-8eb5-f32591ffc86d>
2007-01-01 03:06:29 +0000

    mysql.class.php: Bei allen Methoden return anweisungen true|false plus error_text
    git-svn-id: svn://svn.berlios.de/jclubcms@25 688d4dae-421b-0410-8eb5-f32591ffc86d

--- trunk/modules/mysql.class.php
+++ trunk/modules/mysql.class.php
@@ -40,16 +40,19 @@
 *-----------------------------------------------------------------*/
 
 class mysql {
-		
+
 	private $mysql_server;
 	private $mysql_db;
 	private $mysql_user;
 	private $mysql_pw;
 	private $new_c;
-	
+
 	private $server_link;
 	private $result;
-	
+
+	private $error_text;
+	private $error_no;
+
 	/**
 	 * Das Konstrukt dieser Klasse
 	 *
@@ -59,9 +62,9 @@ class mysql {
 	 * @param string $pw
 	 * @param string $newcon
 	 */
-	
+
 	function __construct($server, $name, $user, $pw, $newcon = 1) {
-		
+
 		$this->mysql_server = $server;
 		$this->mysql_db = $name;
 		$this->mysql_user = $user;
@@ -69,121 +72,155 @@ class mysql {
 		$this->new_c = $newcon;
 		$this->connect();
 	}
-	
+
 	/**
 	 * Öffnet die Verbindung zum Server (wird beim Erstellen des Objekts
 	 * aufgerufen
 	 *
 	 */
-	
+
 	private function connect() {
-		
+
 		$this->server_link = mysql_connect($this->mysql_server, $this->mysql_user, $this->mysql_pw, $this->new_c);
-		mysql_select_db($this->mysql_db, $this->server_link) or $this->error(mysql_errno(), mysql_error());
+		if(!is_resource($this->server_link)) {
+			$this->error_text = mysql_error();
+			$this->error_no = mysql_errno();
+			return false;
+		}
+
+		if(!mysql_select_db($this->mysql_db, $this->server_link)) {
+			$this->error_text = mysql_error();
+			$this->error_no = mysql_errno();
+			return false;
+		} else {
+			return true;
+		}
+
 	}
-	
+
 	/**
 	 * Sendet eine Anfrage an MySQL
 	 *
 	 * @param string $query Die Mysql-Eingabe
 	 */
-	
+
 	public function query($query) {
-		$this->result = mysql_query($query, $this->server_link) or $this->error(mysql_errno(), mysql_error());
+		$this->result = @mysql_query($query, $this->server_link);
+
+		if(!is_resource($this->result)) {
+			$this->error_text = mysql_error($this->server_link);
+			$this->error_no = mysql_errno($this->server_link);
+			return false;
+		} else {
+			return true;
+		}
 	}
-	
+
 	/**
 	 * Liefert einen Datensatz als Array
 	 * 
 	 * @param string[optional] $resulttype = "both"|"num"|"assoc"
 	 * @return array|boolean $data|false
 	 */
-	
+
 	public function fetcharray($resulttype = "both") {
-		
+
 		switch ($resulttype)
 		{
 			case "num":
-			$type = MYSQL_NUM;
-			break;
-			
+				$type = MYSQL_NUM;
+				break;
+
 			case "assoc":
-			$type = MYSQL_ASSOC;
-			break;
-			
+				$type = MYSQL_ASSOC;
+				break;
+
 			default:
-			$type = MYSQL_BOTH;				
+				$type = MYSQL_BOTH;
 		}
-		
-		if(is_resource($this->result)) {
-			$data = mysql_fetch_array($this->result, $type);
-		} else {
-			return false;
-		}
-		
+
+		$data = @mysql_fetch_array($this->result, $type);
+
+
 		if(is_array($data)) {
 			return $data;
 		} else {
+			$this->error_text = mysql_error($this->server_link);
+			$this->error_no = mysql_errno($this->server_link);
 			return false;
 		}
-		
-		
+
 	}
-	
+
 	/**
 	 * Liefert die Anzahl der Datensätze im Ergebnis
 	 *
 	 * @return int
 	 */
-	
+
 	public function num_rows() {
-		$number = mysql_num_rows($this->result);
+		$number = @mysql_num_rows($this->result, $type);
+		if($number == false) {
+			$this->error_text = mysql_error($this->server_link);
+			$this->error_no = mysql_errno($this->server_link);
+			return false;
+		}
 		return $number;
+
 	}
-	
+
 	/**
 	 * Liefert die Anzahl betroffener Datensätze einer vorhergehenden MySQL Operation
 	 *
 	 * @return int
 	 */
-	
+
 	public function affected_rows() {
 		$number = mysql_affected_rows($this->result);
+		if($number == false) {
+			$this->error_text = mysql_error($this->server_link);
+			$this->error_no = mysql_errno($this->server_link);
+			return false;
+		}
 		return $number;
 	}
 	
-	/**
-	 * Gibt die Fehler aus
-	 *
-	 * @param string $errorcode
-	 * @param string $errortext
-	 */
-	private function error($errorcode, $errortext) {
-
-	$errorstring = "Die Datenbankverbindung wurde wegen Fehlern abgebrochen!<br />\n"
-				  ."Fehlercode: $errorcode<br />\n"
-				  ."Fehlernachricht: $errortext";
-	
-	die($errorstring);
+	public function get_error() {
+		return array($this->error_no, $this->error_text);
 	}
-	
+
+
 	/**
 	 * Trennt die Verbindung zur Datenbank
 	 * (@link mysql_close)
 	 *
 	 */
-	
-	private function disconnect() {
-		mysql_close($this->server_link);
+
+	public function disconnect() {
+		if(mysql_close($this->server_link))
+		{
+			$this->error_text = mysql_error($this->server_link);
+			$this->error_no = mysql_errno($this->server_link);
+			return false;
+		}
+
+		$this->__destruct();
 	}
-	
+
 	/**
 	 * Der Destruktor dieser Klasse
 	 * (@link $this->disconnect)
 	 */
-	
+
 	public function __destruct() {
-		$this->disconnect();
+		$this->mysql_server = null;
+		$this->mysql_db = null;
+		$this->mysql_user = null;
+		$this->mysql_pw = null;
+		$this->new_c = null;
+		$this->server_link = null;
+		$this->result = null;
+
 	}
 
 };
